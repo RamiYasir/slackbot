0b262f5ffd83a4d78668e2ac3818a479d520276d
Rami <ramimilkyasir@gmail.com>
2026-07-02 16:50:37 +0100

    add request validator to gateway method

--- infrastructure/modules/api_gateway/main.tf
+++ infrastructure/modules/api_gateway/main.tf
@@ -15,13 +15,41 @@ resource "aws_api_gateway_resource" "add_email" {
     path_part   = "add-email"
 }
 
+resource "aws_api_gateway_model" "email_model" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    name        = "EmailModel"
+    content_type = "application/json"
+    schema = jsonencode({
+        type = "object"
+        properties = {
+            email = {
+                type = "string"
+            }
+        }
+        required = ["email"]
+    })
+
+}
+
 
 # POST method
+resource "aws_api_gateway_request_validator" "request_validator" {
+    rest_api_id = aws_api_gateway_rest_api.api.id
+    name        = "validator"
+    validate_request_body = true
+    validate_request_parameters = false
+}
+
 resource "aws_api_gateway_method" "add_email" {
     rest_api_id   = aws_api_gateway_rest_api.api.id
     resource_id   = aws_api_gateway_resource.add_email.id
     http_method   = "POST"
     authorization = "NONE"
+
+    request_validator_id = aws_api_gateway_request_validator.request_validator.id
+    request_models = {
+        "application/json" = aws_api_gateway_model.email_model.name
+    }
 }
 
 # Defines what is sent to the lambda
